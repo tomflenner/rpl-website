86c45abe699d2f439d009f14f983666ce6edba67
b4cktr4ck5r3 <flennertom@gmail.com>
2022-11-11 19:51:32 +0100

    fix mouse on rule element

--- src/components/Rules/RuleElement.tsx
+++ src/components/Rules/RuleElement.tsx
@@ -20,17 +20,17 @@ export const RuleElement = ({ title, description }: RuleElementProps) => {
   const handleToggle = () => setShow(!show);
 
   return (
-    <VStack border="1px" borderRadius="5" _hover={{
+    <VStack
+      border="1px"
+      borderRadius="5"
+      onClick={handleToggle}
+      cursor="pointer"
+      _hover={{
         borderColor: "orange",
-        color: "orange"
-    }}>
-      <Stack
-        direction="row"
-        w="65vw"
-        h="5vh"
-        cursor="pointer"
-        onClick={handleToggle}
-      >
+        color: "orange",
+      }}
+    >
+      <Stack direction="row" w="65vw" h="5vh">
         <Flex alignItems="center" justify="flex-start" flex="1">
           <Text marginLeft="5">{title}</Text>
           <Icon
@@ -40,11 +40,13 @@ export const RuleElement = ({ title, description }: RuleElementProps) => {
           />
         </Flex>
       </Stack>
-      <Collapse in={show}>
-        <Box w="65vw" padding="5">
-          <Text>{description}</Text>
-        </Box>
-      </Collapse>
+      <Box>
+        <Collapse in={show}>
+          <Box w="65vw" padding="5">
+            <Text>{description}</Text>
+          </Box>
+        </Collapse>
+      </Box>
     </VStack>
   );
 };
