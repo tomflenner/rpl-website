b59850eeb89660ef5365764650121ad63b0fd7c3
b4cktr4ck5r3 <flennertom@gmail.com>
2022-11-11 14:07:37 +0100

    update package.json

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,15 @@
 import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { BaseBreakpointConfig, Breakpoints } from '@chakra-ui/theme-tools'
 
 const fonts = { mono: `'Menlo', monospace` }
 
-const breakpoints = createBreakpoints({
+const breakpoints : Breakpoints<BaseBreakpointConfig> = {
+  base: '0em',
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-})
+}
 
 const theme = extendTheme({
   semanticTokens: {
