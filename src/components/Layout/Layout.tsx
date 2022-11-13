6cf9c8b9f83ec8f5973a79320558f08e94bc0ab3
b4cktr4ck5r3 <flennertom@gmail.com>
2022-11-13 15:43:17 +0100

    fix non auth user

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,7 +12,7 @@ export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <>
-      <Navbar user={data?.passport.user} />
+      <Navbar user={data?.passport?.user} />
       <main>{children}</main>
       <Footer />
     </>
